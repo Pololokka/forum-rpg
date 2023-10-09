bb175572777838018ec13383c09cbb7fe9e2f3e0
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-10-09 16:16:39 -0300

    :construction: feat: começando estilização global

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './App.css';
 function App() {
   return (
     <main>
-      <section>
+      <section className="outer-card">
         <h1 className="font-title font-big">Teste do Título</h1>
-        <p className="font-text font-med">Teste do Texto</p>
-        <p className="font-input font-small">Teste do Texto de Input</p>
+        <div className="inner-card">
+          <p className="font-text font-med">Teste do Texto</p>
+          <p className="font-input font-small">Teste do Texto de Input</p>
+        </div>
       </section>
     </main>
   );
