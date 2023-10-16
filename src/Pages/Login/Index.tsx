36c99c44b95ae2e2a27921226d09445d051d9be2
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-10-16 18:51:37 -0300

    :construction: feat: fazendo login

--- src/Pages/Login/Index.tsx
+++ src/Pages/Login/Index.tsx
@@ -1,4 +1,31 @@
+import './Styles.css';
+
+import { useState } from 'react';
+
+const defaultLogin: { userName: string; password: string } = {
+  userName: '',
+  password: '',
+};
+
 const Login = () => {
+  const [login, setLogin] = useState(defaultLogin);
+
+  const handleLogin = (event: unknown) => {
+    event.preventDefault();
+    console.log(login);
+  };
+
+  const handleChangeInput = (
+    event: unknown,
+    state: typeof login,
+    setState: typeof setLogin,
+  ) => {
+    const name: string = event.target.name;
+    const value: string = event.target.value;
+
+    setState({ ...state, [name]: value });
+  };
+
   return (
     <>
       <header>
@@ -6,7 +33,42 @@ const Login = () => {
       </header>
 
       <main>
-        <section></section>
+        <section>
+          <form
+            className="outer-card form-card"
+            onSubmit={(event) => handleLogin(event)}
+          >
+            <label htmlFor="userName" className="font-text font-med">
+              Usuário
+            </label>
+            <input
+              type="text"
+              name="userName"
+              id="userName"
+              className="font-text font-med input-field"
+              value={login.userName || ''}
+              onChange={(event) => handleChangeInput(event, login, setLogin)}
+            />
+
+            <label htmlFor="password" className="font-text font-med">
+              Senha
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="font-text font-med input-field"
+              value={login.password || ''}
+              onChange={(event) => handleChangeInput(event, login, setLogin)}
+            />
+
+            <input
+              type="submit"
+              value="Entrar!"
+              className="font-title font-small input-button-positive"
+            />
+          </form>
+        </section>
       </main>
     </>
   );
