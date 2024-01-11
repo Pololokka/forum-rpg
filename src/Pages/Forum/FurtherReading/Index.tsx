7c85289ce3d7d09739d3e06a191b61705e4e74de
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2024-01-11 11:01:24 -0300

    :sparkles: feat: feita integração da API na aba de Further Reading

--- src/Pages/Forum/FurtherReading/Index.tsx
+++ src/Pages/Forum/FurtherReading/Index.tsx
@@ -1,19 +1,38 @@
 import '../Styles.css';
 import { ForumBarData } from '../../../Data/SideBarData';
-import { mockedData } from '../../../Data/MockedData';
 
 import Card from '../../../Components/Card/Index';
 import SideBar from '../../../Components/SideBar/Index';
+import PageCount from '../../../Components/PageCount/Index';
+import LimitChanger from '../../../Components/LimitChanger/Index';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
+import { UserConsumer } from '../../../Contexts/User';
+import useFetch from '../../../Hooks/useFetch';
 
 const FurtherReading = () => {
-  const [newPostText, setNewPostText] = useState('');
-
   const { id }: any = useParams();
+  const { userInfo }: any = UserConsumer();
+
+  const [newPostText, setNewPostText] = useState('');
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(10);
+
+  const [posts, pages, pageArray, { reFetch, postData }] = useFetch(
+    `https://forum-rpg-back.onrender.com/api/reading/posts/${id}?offset=${offset}&limit=${limit}`,
+  );
 
   const handleSubmitPost = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    postData(
+      userInfo,
+      id,
+      newPostText,
+      `https://forum-rpg-back.onrender.com/api/reading`,
+    );
+    setNewPostText('');
+    setOffset((pages - 1) * 10);
+    reFetch();
   };
 
   const handleForumChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -22,22 +41,70 @@ const FurtherReading = () => {
 
   return (
     <main className="main__forum">
-      <h1 className="font-title font-big forum__title">Leitura Geral</h1>
+      <h1 className="font-title font-big forum__title">Fórum de Discussões</h1>
 
       <SideBar arrayData={ForumBarData} />
 
       <section>
-        {mockedData[id].posts.map((element, index) => {
+        {posts?.map((element: any, index: number) => {
           return (
             <Card
               key={index}
-              photo={element.photo}
+              photo={element.profilePic}
               user={element.user}
-              date={element.date}
+              date={element.createdAt
+                .split('T')[0]
+                .split('-')
+                .reverse()
+                .join('-')}
               postContent={element.postContent}
             />
           );
         })}
+      </section>
+
+      {pages > 1 && (
+        <section className="changers-numbers">
+          {offset != 0 && (
+            <PageCount
+              type="prev"
+              content="<"
+              offset={offset}
+              limit={limit}
+              setOffset={setOffset}
+            />
+          )}
+
+          {pageArray?.map((element: any, index: number) => {
+            return (
+              <PageCount
+                type="number"
+                content={element}
+                offset={offset}
+                limit={limit}
+                setOffset={setOffset}
+                key={index}
+              />
+            );
+          })}
+
+          {offset != (pages - 1) * 10 && (
+            <PageCount
+              type="next"
+              content=">"
+              offset={offset}
+              limit={limit}
+              setOffset={setOffset}
+            />
+          )}
+        </section>
+      )}
+
+      <section className="changers-numbers">
+        <p className="font-text font-med">Posts por página: </p>
+        <LimitChanger content="10" limit={limit} setLimit={setLimit} />
+        <LimitChanger content="20" limit={limit} setLimit={setLimit} />
+        <LimitChanger content="30" limit={limit} setLimit={setLimit} />
       </section>
 
       <section>
